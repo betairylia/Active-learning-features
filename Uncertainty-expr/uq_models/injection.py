e5520b3c342e7677059319c3f03616d868adf207
betairylia <betairya@gmail.com>
2024-03-09 18:09:12 +0900

    InjectTest impl

--- Uncertainty-expr/uq_models/injection.py
+++ Uncertainty-expr/uq_models/injection.py
@@ -15,3 +15,30 @@ class InjectTest(SimpleModel):
         self.combined_net_init = nn.Sequential(self.net_init, self.head_init)
 
         # breakpoint()
+
+    def forward(self, x):
+
+        if self.training:
+            disable_perturb(self.combined_net)
+            logits = self.combined_net(x)
+            return logits, None
+        
+        else:
+            resample_perturb(self.combined_net)
+            enable_perturb(self.combined_net)
+            logits = []
+            probs = []
+
+            for i in range(self.args.dropout_iters):
+                logits.append(self.head(self.net(x)))
+                pred_prob = F.softmax(logits[-1], dim = 1)
+                probs.append(pred_prob)
+
+            logits = torch.stack(logits, dim = 0)
+            probs = torch.stack(probs, dim = 0)
+
+            model_prediction = probs.mean(0)
+            entropy = -torch.sum(model_prediction * torch.log(model_prediction + 1e-8), dim = 1)
+            uncertainty = entropy
+
+            return logits.mean(dim = 0), uncertainty
--- Uncertainty-expr/uq_models/param_inject.py
+++ Uncertainty-expr/uq_models/param_inject.py
@@ -25,11 +25,37 @@ def InjectNet(net, *args, **kwargs):
     
     return net
 
+def enable_perturb(module, *args, **kwargs):
+    for each_module in m.modules():
+        if isinstance(each_module, ParameterInjector):
+            each_module.enable(*args, **kwargs)
+
+def disable_perturb(module, *args, **kwargs):
+    for each_module in m.modules():
+        if isinstance(each_module, ParameterInjector):
+            each_module.disable(*args, **kwargs)
+
+def resample_perturb(module, *args, **kwargs):
+    for each_module in m.modules():
+        if isinstance(each_module, ParameterInjector):
+            each_module.sample(*args, **kwargs)
+
 ####################################
 ## ↓↓ Injector implementations ↓↓ ##
 ####################################
 
-class Linear_ParameterInjector(nn.Module):
+class ParameterInjector(nn.Module):
+
+    def enable(self, *args, **kwargs):
+        pass
+    
+    def disable(self, *args, **kwargs):
+        pass
+    
+    def sample(self, *args, **kwargs):
+        pass
+
+class Linear_ParameterInjector(ParameterInjector):
 
     def __init__(self, moduleToWrap):
         
@@ -43,9 +69,33 @@ class Linear_ParameterInjector(nn.Module):
         # bias
         self.bias_inject = torch.zeros_like(self.module.bias)
         self.bias_original = self.module.bias
+
+        self.enabled = False
+
+    def enable(self, *args, **kwargs):
+        print("Enabled Linear_PI")
+        self.enabled = True
     
+    def disable(self, *args, **kwargs):
+        print("Disabled Linear_PI")
+        self.enabled = False
+    
+    def sample(self, *args, **kwargs):
+        self.weight_inject = torch.normal(
+            mean = torch.ones_like(self.module.weight),
+            std = self.module.weight * 0.1,
+            device = self.module.weight.device)
+        
+        # TODO: bias
+
     def forward(self, x):
 
-        self.module.weight = self.weight_original + self.weight_inject
-        self.module.bias = self.bias_original + self.bias_inject
-        return self.module(x)
+        if self.enabled:
+            self.module.weight = self.weight_original + self.weight_inject
+            self.module.bias = self.bias_original + self.bias_inject
+            return self.module(x)
+
+        else:
+            self.module.weight = self.weight_original
+            self.module.bias = self.bias_original
+            return self.module(x)
